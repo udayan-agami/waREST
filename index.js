a694d532e2fc82e3d9a44f82ec7d4931ff530dc5
Udayan Basak <thisisudayan@gmail.com>
2024-03-22 02:02:11 +0600

    af

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
 const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
+const { createServer } = require('http');
+const { join } = require('path');
 const { Server } = require('socket.io');
 const { Client } = require('whatsapp-web.js');
 const _ = require('lodash');
-
+const bodyParser = require('body-parser');
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+// Initialize WhatsApp client
+const client = new Client();
+client.on('qr', (qr) => {
+    // Emit QR code to clients
+    io.emit('qr', qr);
+});
+client.once('ready', () => {
+    // Emit ready status to clients
+    io.emit('ready', true);
+});
+client.initialize();
 
-
+// Socket.IO connection event
 io.on('connection', (socket) => {
-    const client = new Client();
-    client.on('qr', (qr) => {
-        socket.emit('qr', qr);
-    });
-    client.once('ready', () => {
-        socket.emit('ready', true);
-    });
-    client.initialize();
-
-    io.on('sendMessage', async(d) => {
-        await client.sendMessage(d.phone, d.message);
-    })
+    // Handle socket events if needed
 });
 
-
-
+// Express route for serving HTML file
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+    res.sendFile(join(__dirname, 'index.html'));
 });
 
-
-app.get('/message', (req, res) => {
-  const phone = req.query.phone ?? '';
-  const message = req.query.message ?? '';
-  if(_.isEmpty(phone) || _.isEmpty(message)) {
-    res.status(403).json({error: true, message: 'Phone number or message is not valid'});
-  } else {
-    io.emit('sendMessage', { phone: phone, message: message });
-    res.status(200).json({error: false, message: 'Message sent successfully'});
-  }
+// Express route for sending WhatsApp message
+app.post('/message', async (req, res) => {
+    const phone = req.body.phone ?? '';
+    const message = req.body.message ?? '';
+    if (_.isEmpty(phone) || _.isEmpty(message)) {
+        res.status(500).json({ error: true, message: 'Phone number or message is not valid' });
+    } else {
+        try {
+            await client.sendMessage(`${phone}@c.us`, message);
+            res.status(200).json({ error: false, message: 'Message sent successfully' });
+        } catch (error) {
+            res.status(500).json({ error: true, message: 'Failed to send message' });
+        }
+    }
 });
 
-
-
+// Start the server
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+    console.log('Server running at http://localhost:3000');
 });
