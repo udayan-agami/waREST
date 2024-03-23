ea8a10b9893ba048925d6cc6acd77fa9e80659e7
AN Udayan <thisisudayan@gmail.com>
2024-03-23 15:09:37 +0600

    commit

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const express = require('express');
 const { createServer } = require('http');
 const { join } = require('path');
 const { Server } = require('socket.io');
-const { Client } = require('whatsapp-web.js');
+const { Client, LocalAuth  } = require('whatsapp-web.js');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+require('dotenv').config();
+
 // Initialize WhatsApp client
-const client = new Client();
+const client = new Client({
+    
+    authStrategy: new LocalAuth({
+        clientId: 'client-one',
+        dataPath: 'sessionstore'
+    })
+});
 client.on('qr', (qr) => {
     // Emit QR code to clients
     io.emit('qr', qr);
@@ -19,12 +27,17 @@ client.on('qr', (qr) => {
 client.once('ready', () => {
     // Emit ready status to clients
     io.emit('ready', true);
+    console.log('Whatsapp connected and ready');
 });
 client.initialize();
 
 // Socket.IO connection event
 io.on('connection', (socket) => {
-    // Handle socket events if needed
+    if(client.info == undefined) {
+        socket.emit('info', 'Please Go to Whatsapp from your phone and linked device scanning this QR code.')
+    } else {
+        socket.emit('info', `${client.info.pushname} (${client.info.wid.user}) is connected`)
+    }
 });
 
 // Express route for serving HTML file
@@ -36,13 +49,19 @@ app.get('/', (req, res) => {
 app.post('/message', async (req, res) => {
     const phone = req.body.phone ?? '';
     const message = req.body.message ?? '';
+    const key = req.body.key?? '';
+    const serverkey = process.env.APIKEY;
+
     if (_.isEmpty(phone) || _.isEmpty(message)) {
         res.status(500).json({ error: true, message: 'Phone number or message is not valid' });
+    } else if (key != serverkey) {
+        res.status(500).json({error: true, message: 'Wrong API key'})
     } else {
         try {
             await client.sendMessage(`${phone}@c.us`, message);
             res.status(200).json({ error: false, message: 'Message sent successfully' });
         } catch (error) {
+            console.log(error);
             res.status(500).json({ error: true, message: 'Failed to send message' });
         }
     }
